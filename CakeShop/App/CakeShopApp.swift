df0b1eaf2304c807bb3651f4a14180590997b651
Darina Samokhina <mokhinsam@icloud.com>
2025-02-12 16:27:19 +0300

    Initial commit

---
+++ CakeShop/App/CakeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeShopApp.swift
+//  CakeShop
+//
+//  Created by Дарина Самохина on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CakeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
